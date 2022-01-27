4b7642a91919ef330861e9c6cd4a3f9cae1a81f4
IDRIS GBENGA <gbenga@crowdforce.io>
2022-01-27 10:25:20 +0100

    Add project files.

---
+++ WinServiceManager/Controller/ManageController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WinServiceManager.BLL;
+
+namespace WinServiceManager.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManageController : ControllerBase
+    {
+        private readonly IWindowServiceMgr serviceMgr;
+
+        public ManageController(IWindowServiceMgr serviceMgr)
+        {
+            this.serviceMgr = serviceMgr;
+        }
+        [HttpGet(nameof(StartService))]
+        public IActionResult StartService()
+        {
+            try
+            {
+                serviceMgr.StartOrRestart();
+                return Ok("service started successfully!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"{e.InnerException}");
+            }
+        }
+        [HttpGet(nameof(StopService))]
+        public IActionResult StopService()
+        {
+            try
+            {
+               // serviceMgr.StopServiceIfRunning();
+                return BadRequest("stop service operation not allow!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"{e.InnerException}");
+            }
+        }
+
+    }
+}
